e8644ba0b140f4735fedd6afadfd82f40aa5fcb0
Tu Bean <tubean.81@gmail.com>
2018-12-19 15:58:58 +0700

    create new service for authentication by JWT

---
+++ auth-service/src/main/java/io/github/tubean/authservice/AuthServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.tubean.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
+
---
+++ config/src/test/java/io/github/tubean/config/ConfigApplicationTests.java
@@ -0,0 +1,17 @@
+package io.github.tubean.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
